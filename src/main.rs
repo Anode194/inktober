6df6049f4de8892c2af5cef9dc472d3a02bf5d17
Jo Phillips <joxphillips@gmail.com>
2020-10-02 21:32:07 -0600

    Oct 2 Wisp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nannou::color::*;
 use nannou::prelude::*;
+use nannou::geom::rect::Rect;
 fn main() {
     nannou::app(model).update(update).simple_window(view).run();
     println!("Hello, world!");
@@ -10,54 +11,42 @@ fn model(_app: &App) -> Model {
 }
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-struct Fish {
-    center: Point2,
-    tail_b: Point2,
-    tail_t: Point2,
-
-    head_x: f32,
-    head_y: f32,
-    head_w: f32,
-    head_h: f32,
-    color: Rgb<u8>,
-}
-impl Fish {
-    fn new() -> Fish {
-        Fish {
-            head_x: 80.0,
-            head_y: 0.0,
-            head_w: 180.0,
-            head_h: 90.0,
-            center: pt2(0.0, 0.0),
-            tail_b: pt2(-120.0, -60.0),
-            tail_t: pt2(-120.0, 60.0),
-            color: rgb_u32(0x8d0054),
-        }
-    }
-}
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
-    //background from hex
     let bg = rgb_u32(0x001726);
-    let f = Fish::new();
+    let sine = app.time.sin();
+    let slowersine = (app.time / 2.0).sin();
+    let boundary = app.window_rect();   
+
+    let x = map_range(sine, -1.0,1.0, boundary.left()+80.0, boundary.right() -80.0);
+    let y = map_range(slowersine, -1.0, 1.0, boundary.bottom()+80.0, boundary.top()-80.0);
     draw.background().color(bg);
-    draw.tri()
-        .color(f.color)
-        .points(f.tail_b, f.tail_t, f.center);
     draw.ellipse()
-        .color(f.color)
-        .x_y(f.head_x, f.head_y)
-        .w(f.head_w)
-        .h(f.head_h);
-    //eyeball
-    draw.ellipse().color(CYAN).x_y(120.0, 8.0).w(10.0).h(10.0);
-    //gills
-    draw.tri()
-        .points(pt2(100.0, 0.0), pt2(70.0, 8.0), pt2(70.0, -8.0))
-        .color(BLUE);
-    draw.text("a fish named todd")
+        .color(WHITE)
+        .w_h(70.0,60.0)
+        .x_y(x,y);
+    //    let sheet:Rect = Rect::from_x_y_w_h(x,y,15.0,45.0);
+    let sheet_y = y -25.0;
+    draw.rect()
+        .w_h(70.0,45.0)
+        .x_y(x,sheet_y);
+    draw.text("wisp oct 2 2020")
         .x_y(0.0, 200.0)
         .font_size(18);
+    let left_eye = x - 10.0;
+    let right_eye = x + 10.0;
+
+
+    draw.rect()
+        .w_h(5.0,10.0)
+        .x_y(left_eye,y)
+        .color(bg);
+    draw.rect()
+        .w_h(5.0,10.0)
+        .x_y(right_eye,y)
+        .color(bg);
+    
 
     draw.to_frame(app, &frame).unwrap();
+
 }
